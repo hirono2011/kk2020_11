8c44213a3205a4eace904c0ced9be576040c5fe6
XTe-PC@ubuntu2004 <hirono2013k@gmail.com>
2021-03-31 12:23:47 +0900

    new file:   告発状2021/2020-08-01_告発状（未提出）の告発の事実部分.md 	deleted:    告発状2021/2021-02-03-142048_告発に至る経緯・鬼滅の刃と，弁護士列車編.md 	deleted:    告発状2021/2021-02-03-142048_告発に至る経緯・鬼滅の刃と，弁護士列車編.pdf 	deleted:    告発状2021/2021-02-03-142048_告発に至る経緯・鬼滅の刃と，弁護士列車編.tex~ 	deleted:    告発状2021/2021-02-04-114436_告発に至る経緯・奥能登の蛸島事件と，弁護士列車編.md 	deleted:    告発状2021/2021-02-04-114436_告発に至る経緯・奥能登の蛸島事件と，弁護士列車編.pdf 	deleted:    告発状2021/2021-02-04-114650_告発の事実・弁護士列車編.md 	deleted:    告発状2021/2021-02-04-114650_告発の事実・弁護士列車編.pdf 	modified:   告発状2021/2021-02-27-083344_告発状2021-03.md 	new file:   告発状2021/2021-02-27-083344_告発状2021-03.pdf 	new file:   告発状2021/2021-03-31_令和3年3月31日付け　告発状.pdf 	deleted:    告発状2021/a.aux 	deleted:    告発状2021/a.log 	modified:   告発状2021/a.md 	modified:   告発状2021/a.pdf 	deleted:    告発状2021/a.tex 	deleted:    告発状2021/a.toc 	deleted:    告発状2021/b.aux 	deleted:    告発状2021/b.dvi 	deleted:    告発状2021/b.log 	deleted:    告発状2021/b.pdf 	deleted:    告発状2021/b.tex 	deleted:    告発状2021/b.toc 	deleted:    告発状2021/c.aux 	deleted:    告発状2021/c.log 	deleted:    告発状2021/c.pdf 	deleted:    告発状2021/image-1.png 	modified:   告発状2021/make-pdf.sh 	modified:   告発状2021/preamble.txt 	deleted:    告発状2021/t/2021-02-04-114436_告発に至る経緯・奥能登の蛸島事件と，弁護士列車編.md 	deleted:    告発状2021/t/2021-02-04-114436_告発に至る経緯・奥能登の蛸島事件と，弁護士列車編.pdf 	deleted:    告発状2021/t/a.md 	deleted:    告発状2021/t/make-pdf.sh 	deleted:    告発状2021/t/preamble.txt 	deleted:    告発状2021/t/tmp.md 	modified:   告発状2021/tmp.aux 	deleted:    告発状2021/tmp.dvi 	modified:   告発状2021/tmp.log 	modified:   告発状2021/tmp.md 	new file:   告発状2021/tmp.pdf 	modified:   告発状2021/tmp.tex 	deleted:    告発状2021/tmp.tex~ 	modified:   告発状2021/tmp.toc 	new file:   告発状2021/tmp.txt 	deleted:    告発状2021/告発状-1.jpg 	deleted:    告発状2021/告発状.aux 	deleted:    告発状2021/告発状.log 	deleted:    告発状2021/告発状.pdf 	deleted:    告発状2021/告発状.tex 	deleted:    告発状2021/告発状.toc
    update 2021-03-31_1223 a66-XTe

--- 告発状2021/make-pdf.sh
+++ 告発状2021/make-pdf.sh
@@ -9,6 +9,7 @@ sed -i -E 's/^>\|.+\|/\\begin{lstlisting}/' tmp.md
 sed -i -E 's/\|\|<$/\\end{lstlisting}/' tmp.md
 sed -i 's/\\n/￥＼n/g' tmp.md
 sed -i -E 's/^(- .+)\[(https?:\/\/.+)\]\(https?:\/\/.+\)$/\1\2/g' tmp.md
+sed -i -E 's/(〉〉〉　kk_hironoのリツイート　〉〉〉).*$/\1\n\n/mg' tmp.md
 #sed -i -e 's%\(https\?://[^ ]\+\)[ \|"]% <\1>%g' tmp.md
 #sed -i -E "s%(https?:\/\/[-_.-p\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)% <\1>%g" tmp.md
 
@@ -21,6 +22,9 @@ sed -i -E 's/"(令和.+)\1/' preamble.txt
 sed -i -E "/\\maketitle/r preamble.txt" tmp.tex
 sed -i -E "/\\makeatother/r preamble_listings.txt" tmp.tex
 sed -i -E "1,/\\begin\{document\}/s/(https?:\/\/[-_.-p\'()a-zA-Z0-9;\/?:\@&=+\$,%#]+)/\\\url{\1} /g" tmp.tex
+sed -i -E 's/\\\\}/}/' tmp.tex
+sed -i -E 's/\\textbackslash\\textgreater\{\}/\\par\\textgreater\{\}/' tmp.tex
+
 
 lualatex tmp.tex
 lualatex tmp.tex
